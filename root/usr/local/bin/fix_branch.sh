180608221d49d9986b41aed4d51a504c4f6b3cd2
root <root@ns338919.ip-178-33-224.eu>
2021-04-15 10:41:39 +0200

    build

--- root/usr/local/bin/fix_branch.sh
+++ root/usr/local/bin/fix_branch.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 B=$1
-echo "${B}"
+
+cat >/etc/yum.repos.d/metwork.repo <<EOF
+[metwork]
+name=MetWork
+baseurl=http://metwork-framework.org/pub/metwork/continuous_integration/rpms/${B}/portable/
+gpgcheck=0
+enabled=1
+metadata_expire=0
+EOF
+
+if test "$B" = "release_0.9"; then
+  cat /etc/cont-init.d/download_mfserv_plugins |sed "s~@@@PLUGINS@@@~external_plugins~g" >/etc/cont-init.d/download_mfserv_plugins.new
+else
+  cat /etc/cont-init.d/download_mfserv_plugins |sed "s~@@@PLUGINS@@@~plugins~g" >/etc/cont-init.d/download_mfserv_plugins.new
+fi
+mv -f /etc/cont-init.d/download_mfserv_plugins.new /etc/cont-init.d/download_mfserv_plugins
